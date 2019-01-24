4a1302395b5ff65d0a40447e3156ed13ebe452ff
Unknown <DTruong054@west-mec.org>
2019-01-24 13:51:17 -0800

    Interupted

--- index.js
+++ index.js
@@ -6,13 +6,17 @@
 
 var express = require('express');
 var app = express();
-var port = 5500;
+var config = require('./config.json'); //Including the JSON file
+var url = require('url');
+
+//The "app.use" need a function. The require('cookie-parser') fills in for the function, and the () after are needed for parameteres. 
+app.use(require('cookie-parser')());
 
 app.get('/', function (req, res) {
-    res.send("<h3>Hello world</h3>");
+    res.send("<h3>Hello world</h3>"); //Sends the message in a h3
 });
 
 //Building a server to listen on port 8080
-app.listen(port, function () {
-    console.log("Server is listening on localhost port:%s", port);
+app.listen(config.port, function () {
+    console.log("Server is listening on localhost port:%s", config.port);
 });
